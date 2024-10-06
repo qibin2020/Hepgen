7eab9e1b36b0a579d06a483219f3fa790ef58a13
qibin.liu@cern.ch <liuqibin@bl-0.phy.sjtulocal>
2024-10-07 03:06:09 +0800

    Add lxplus env

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
-. /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc11-opt/setup.sh
+. /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc13-opt/setup.sh
 export DISPLAY=
 export STARTDIR=${PWD}
 mkdir -p logs jobs
 # replace the following!!!
-export LOCALMG5=/lustre/collider/liuqibin/MC/QE/MG5_aMC_v3_5_4
-export PILEUP_FILE=/lustre/collider/liuqibin/Project_MET/generation/workarea/pileup/MinBias.pileup
+export LOCALMG5=PATH_TO_MADGRAPH5_DIR/
+export PILEUP_FILE=PATH_TO_PILEUP_FILE.pileup
 #
 export PYTHIA8DATA=${LOCALMG5}/HEPTools/pythia8/share/Pythia8/xmldoc
 export MG5_PROG=${LOCALMG5}/bin/mg5_aMC
--- pileup/special_env.sh
+++ pileup/special_env.sh
@@ -1,6 +1,6 @@
-. /cvmfs/sft.cern.ch/lcg/views/LCG_102b/x86_64-centos7-gcc11-opt/setup.sh
+. /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc13-opt/setup.sh
 # replace the following!!!
-export LOCALDELPHES=/lustre/collider/liuqibin/Project_MET/generation/delphes-master
+export LOCALDELPHES=PATH_TO_DELPHES_DIR/
 #
 export HAS_PYTHIA8=true
 export DISPLAY=
